8135c344355981b85bde62e7e5c34c918d062c9f
Gabriel Freitas Souza <gabrielsouza45@live.com>
2025-03-26 15:25:44 -0300

    initial commit

---
+++ clinica-facil-back/src/main/java/br/app/tads/clinica_facil/ClinicaFacilApplication.java
@@ -0,0 +1,13 @@
+package br.app.tads.clinica_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaFacilApplication.class, args);
+	}
+
+}
---
+++ clinica-facil-back/src/test/java/br/app/tads/clinica_facil/ClinicaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.app.tads.clinica_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
