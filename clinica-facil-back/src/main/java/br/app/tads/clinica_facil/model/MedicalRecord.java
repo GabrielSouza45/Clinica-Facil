76f8b436c82e4b95f9a674cbcdd3ffee69bb56db
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2025-04-11 22:15:43 -0300

    Criação dos modelos
    Adicionado Model das classes: Agenda, Consultation, Exam, MedicalRecord, Report, Revenue

---
+++ clinica-facil-back/src/main/java/br/app/tads/clinica_facil/model/MedicalRecord.java
@@ -0,0 +1,93 @@
+package br.app.tads.clinica_facil.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//<---------------PRONTUARIO--------------->//
+//<---------------PRONTUARIO--------------->//
+//<---------------PRONTUARIO--------------->//
+
+
+@Entity(name = "medical_record")
+@Table(name = "medical_record")
+@EqualsAndHashCode(of = "id")
+public class MedicalRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient; 
+
+    @OneToMany(mappedBy = "medicalRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Consultation> consultations = new ArrayList<>(); 
+
+    @OneToMany(mappedBy = "medicalRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exam> exams = new ArrayList<>(); 
+
+    @OneToMany(mappedBy = "medicalRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Revenue> revenues = new ArrayList<>(); 
+
+    public MedicalRecord() {}
+
+    public MedicalRecord(Patient patient) {
+        this.patient = patient;
+        this.consultations = new ArrayList<>();
+        this.exams = new ArrayList<>();
+        this.revenues = new ArrayList<>();
+    }
+
+    public MedicalRecord(Long id, Patient patient, List<Consultation> consultations, List<Exam> exams, List<Revenue> revenues) {
+        this.id = id;
+        this.patient = patient;
+        this.consultations = (consultations != null) ? consultations : new ArrayList<>();
+        this.exams = (exams != null) ? exams : new ArrayList<>();
+        this.revenues = (revenues != null) ? revenues : new ArrayList<>();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public List<Consultation> getConsultations() {
+        return consultations;
+    }
+
+    public void setConsultations(List<Consultation> consultations) {
+        this.consultations = consultations;
+    }
+
+    public List<Exam> getExams() {
+        return exams;
+    }
+
+    public void setExams(List<Exam> exams) {
+        this.exams = exams;
+    }
+
+    public List<Revenue> getRevenues() {
+        return revenues;
+    }
+
+    public void setRevenues(List<Revenue> revenues) {
+        this.revenues = revenues;
+    }
+}
--- clinica-facil-back/src/main/java/br/app/tads/clinica_facil/model/Report.java
+++ clinica-facil-back/src/main/java/br/app/tads/clinica_facil/model/Report.java
@@ -1,4 +1,142 @@
 package br.app.tads.clinica_facil.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lombok.EqualsAndHashCode;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity(name = "report")
+@Table(name = "report")
+@EqualsAndHashCode(of = "id")
 public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    private Date issueDate;
+    private String reasons;//Razões para a consulta
+    private String clinicalHistory;//Antecedentes pessoais relevantes(doenças, cirugias, alergias, etc.)
+    private String diagnosis;
+    
+    @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Revenue> revenues = new ArrayList<>();//Medicamentos prescritos, procedimentos realizados ou recomendados, afastamento (se necessário), com CID e período, ancaminhamentos (ex: para especialistas)
+
+    @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exam> exams = new ArrayList<>();
+
+    @OneToOne(mappedBy = "report", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Consultation consultation;
+    
+    public Report(Long id, Patient patient, Doctor doctor, Date issueDate, String reasons, String clinicalHistory,
+        String diagnosis, Consultation consultation, List<Revenue> revenues, List<Exam> exams) {
+        this.id = id;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.issueDate = issueDate;
+        this.reasons = reasons;
+        this.clinicalHistory = clinicalHistory;
+        this.diagnosis = diagnosis;
+        this.consultation = consultation;
+        this.revenues = (revenues != null) ? revenues : new ArrayList<>();
+        this.exams = (exams != null) ? exams : new ArrayList<>();
+    }
+
+    public Report() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Date getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(Date issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getReasons() {
+        return reasons;
+    }
+
+    public void setReasons(String reasons) {
+        this.reasons = reasons;
+    }
+
+    public String getClinicalHistory() {
+        return clinicalHistory;
+    }
+
+    public void setClinicalHistory(String clinicalHistory) {
+        this.clinicalHistory = clinicalHistory;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public List<Revenue> getRevenues() {
+        return revenues;
+    }
+
+    public void setRevenues(List<Revenue> revenues) {
+        this.revenues = revenues;
+    }
+
+    public List<Exam> getExams() {
+        return exams;
+    }
+
+    public void setExams(List<Exam> exams) {
+        this.exams = exams;
+    }    
+
+    
+
 }
