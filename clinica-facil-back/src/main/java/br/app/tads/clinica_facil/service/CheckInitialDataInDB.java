ab6e66486d91eadaa479a886e319e4f4a8d330f9
Gabriel Freitas Souza <gabrielsouza45@live.com>
2025-05-07 16:15:51 -0300

    model front

--- clinica-facil-back/src/main/java/br/app/tads/clinica_facil/service/CheckInitialDataInDB.java
+++ clinica-facil-back/src/main/java/br/app/tads/clinica_facil/service/CheckInitialDataInDB.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 @Service
---
+++ clinica-facil-front/src/app/model/Exam.ts
@@ -0,0 +1,12 @@
+import { Patient } from "./Patient";
+import { MedicalRecord } from "./MedicalRecord";
+
+export class Exam{
+  id?: number;
+  name?: string;
+  description?: string;
+  dateTime?: Date;
+  results?: string;
+  patient?: Patient;
+  medicalRecord?: MedicalRecord;
+}
